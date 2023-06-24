0daef96b62c659f922e1aee5d7251f9b7ae35ce3
Paul Alves <paulalves@protonmail.com>
2023-06-25 01:14:42 +0800

    rework x simplify

---
+++ src/ExpressionCompiler/ExpressionParser.cs
@@ -0,0 +1,22 @@
+﻿namespace ExpressionCompiler
+{
+  public class Scanner
+  {
+  }
+  
+  public class Token
+  {
+  }
+
+  public class TokenKind
+  {
+  }
+
+  public class ExpressionLexer
+  {
+  }
+
+  public class ExpressionParser
+  {
+  }
+}
