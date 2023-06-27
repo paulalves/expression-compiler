e71a74ab541988741c244feb51326f68ed68619b
Paul Alves <paulalves@protonmail.com>
2023-06-28 00:32:55 +0800

    lambda expression compiler

---
+++ src/ExpressionCompiler/Visitors/ExpressionLambdaCompiler.cs
@@ -0,0 +1,47 @@
+namespace ExpressionCompiler.Visitors
+{
+  using System;
+  using System.Linq.Expressions;
+  using ExpressionCompiler.SyntaxTree;
+
+  public class ExpressionLambdaCompiler : IExpressionSyntaxTreeVisitor<LambdaExpression>
+  {
+    public LambdaExpression Visit(AdditionExpressionSyntaxTree syntaxTree)
+    {
+      return Expression.Lambda<Func<decimal>>(
+        Expression.Add(
+          syntaxTree.Lhs.Accept(this).Body,
+          syntaxTree.Rhs.Accept(this).Body));
+    }
+
+    public LambdaExpression Visit(DivisionExpressionSyntaxTree syntaxTree)
+    {
+      return Expression.Lambda<Func<decimal>>(
+        Expression.Divide(
+          syntaxTree.Lhs.Accept(this).Body,
+          syntaxTree.Rhs.Accept(this).Body));
+    }
+
+    public LambdaExpression Visit(MultiplicationExpressionSyntaxTree syntaxTree)
+    {
+      return Expression.Lambda<Func<decimal>>(
+        Expression.Multiply(
+          syntaxTree.Lhs.Accept(this).Body,
+          syntaxTree.Rhs.Accept(this).Body));
+    }
+
+    public LambdaExpression Visit(NumberExpressionSyntaxTree syntaxTree)
+    {
+      return Expression.Lambda<Func<decimal>>(
+        Expression.Constant(syntaxTree.Value));
+    }
+
+    public LambdaExpression Visit(SubtractionExpressionSyntaxTree syntaxTree)
+    {
+      return Expression.Lambda<Func<decimal>>(
+        Expression.Subtract(
+          syntaxTree.Lhs.Accept(this).Body,
+          syntaxTree.Rhs.Accept(this).Body));
+    }
+  }
+}
---
+++ tests/ExpressionCompiler.Tests/Visitors/ExpressionLambdaCompilerTests.cs
@@ -0,0 +1,38 @@
+namespace ExpressionCompiler.Tests.Visitors
+{
+  using System;
+  using System.Linq.Expressions;
+  using ExpressionCompiler.Lexer;
+  using ExpressionCompiler.Parser;
+  using ExpressionCompiler.Visitors;
+  using FluentAssertions;
+  using Xunit;
+
+  public class ExpressionLambdaCompilerTests
+  {
+    [Theory]
+    [InlineData("3/2*1-1*100", -98.5)]
+    [InlineData("3/2*1-1*100+1", -97.5)]
+    [InlineData("3/2*1-1*100+1*2", -96.5)]
+    [InlineData("3/2*1-1*100+1*2/2", -97.5)]
+    [InlineData("3/2*1-1*100+1*2/2+1", -96.5)]
+    [InlineData("3/2*1-1*100+1*2/2+1*2", -95.5)]
+    [InlineData("3/2*1-1*100+1*2/2+1*2/2", -96.5)]
+    [InlineData("3/2*1-1*100+1*2/2+1*2/2+1", -95.5)]
+    [InlineData("3/2*1-1*100+1*2/2+1*2/2+1*2", -94.5)]
+    [InlineData("3/2*1-1*100+1*2/2+1*2/2+1*2/2", -95.5)]
+    [InlineData("3/2*1-1*100+1*2/2+1*2/2+1*2/2+1", -94.5)]
+    public static void InterpreterTests(string exp, decimal expected)
+    {
+      var parser = new ExpressionParser(new ExpressionLexer(new Scanner(exp)));
+      var syntaxTree = parser.Parse();
+      var interpreter = new ExpressionLambdaCompiler();
+
+      var lambda = (Expression<Func<decimal>>)syntaxTree.Accept(interpreter);
+      
+      var func = lambda.Compile();
+
+      func().Should().Be(expected);
+    }
+  }
+}
